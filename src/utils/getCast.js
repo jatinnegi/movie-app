06ddbf3973d0836e0e7f73135401b3daadb4dfa6
Jatin Negi <jatin.07star@gmail.com>
2021-04-05 11:05:52 +0530

    Update default profile photo of cast

--- src/utils/getCast.js
+++ src/utils/getCast.js
@@ -4,7 +4,7 @@ export default function getCast(cast) {
     character: cast.character,
     profile_path: cast.profile_path
       ? `https://www.themoviedb.org/t/p/original${cast.profile_path}`
-      : "https://themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg",
+      : "https://pbs.twimg.com/profile_images/716487122224439296/HWPluyjs_400x400.jpg",
   }));
 
   return actors;
